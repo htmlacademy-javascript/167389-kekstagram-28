718001db3c43b456eb1f1fd0b4ceebc33c99ead5
Eleriniara <eleriniara@gmail.com>
2023-04-10 15:39:52 +0300

    Помощь друга

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ import './miniphoto.js';
 import { findBigPicture, onDocumentKeydown } from './big-picture.js';
 import './form.js';
 import './validation.js';
+import './zoom.js';
+import './effects.js';
+
 allPhotoInfo();
 findBigPicture();
 onDocumentKeydown();
