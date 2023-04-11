e9d1d1f97a288ba39a282e1875861bb63314ca94
Eleriniara <eleriniara@gmail.com>
2023-04-11 22:54:05 +0300

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-import {allPhotoInfo} from './data.js';
-import './miniphoto.js';
+import {pictureAllPhotoInfo} from './miniphoto.js';
 import { findBigPicture, onDocumentKeydown } from './big-picture.js';
 import './form.js';
 import './validation.js';
 import './zoom.js';
 import './effects.js';
+//import { pictureAllPhotoInfo} from './miniphoto.js';
 
-allPhotoInfo();
+// fetch('https://28.javascript.pages.academy/kekstagram/data')
+//   .then((response) => response.json())
+//   .then((similarWizards) => {
+//     renderSimilarList(similarWizards);
+//     console.log(similarWizards);
+//   });
+
+const response = await fetch('https://28.javascript.pages.academy/kekstagram/data');
+const results = await response.json();
+console.log(results[1]);
+
+export { results };
+
 findBigPicture();
 onDocumentKeydown();
+pictureAllPhotoInfo();
--- js/miniphoto.js
+++ js/miniphoto.js
@@ -1,25 +1,17 @@
-import {allPhotoInfo} from './data.js';
-
+import { results } from './main';
 const pictures = document.querySelector('.pictures');
 const picture = document.querySelector('#picture').content.querySelector('.picture');
 
+const pictureAllPhotoInfo = structuredClone(results);
+console.log(pictureAllPhotoInfo);
 
-const pictureAllPhotoInfo = allPhotoInfo();
+// pictureAllPhotoInfo.forEach(({url, likes, comments, id}) => {
+//   const pictureSample = picture.cloneNode(true);
+//   pictureSample.querySelector('.picture__img').src = url;
+//   pictureSample.querySelector('.picture__likes').textContent = likes;
+//   pictureSample.querySelector('.picture__comments').textContent = comments.length;
+//   pictureSample.dataset.pictureId = id;
+//   pictures.appendChild(pictureSample);
+// });
 
-
-pictureAllPhotoInfo.forEach(({url, likes, comments, id}) => {
-  const pictureSample = picture.cloneNode(true);
-  pictureSample.querySelector('.picture__img').src = url;
-  pictureSample.querySelector('.picture__likes').textContent = likes;
-  pictureSample.querySelector('.picture__comments').textContent = comments.length;
-  pictureSample.dataset.pictureId = id;
-  pictures.appendChild(pictureSample);
-});
-
-
-/*Во втором варианте убираю
-const allPhotoInfoPictures = () => Array.from(pictureAllPhotoInfo);
-allPhotoInfoPictures();
-
-и вместо моего кода в bigPicture c forEach ставлю закомментированный код*/
 export {pictures, picture, pictureAllPhotoInfo};
