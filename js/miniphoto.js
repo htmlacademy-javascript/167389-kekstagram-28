18f52d235bfc2bffd41405c0725502c6ecab968b
Eleriniara <eleriniara@gmail.com>
2023-04-16 01:05:53 +0300

    Удалила лишнее

--- js/miniphoto.js
+++ js/miniphoto.js
@@ -1,16 +1,8 @@
 const RANDOM_PICTURE_COAT = 10;
 //const TIME_DELAY = 0.5;
-// const FILTERS = [
-//   DEFAULT: 'filter-default',
-//   RANDOM: 'filter-random',
-//   DISCUSSED: 'filter-discussed'
-// ]
+
 const pictures = document.querySelector('.pictures');
 const picture = document.querySelector('#picture').content.querySelector('.picture');
-// const defaultFilter = document.querySelector('#filter-default');
-// const randomFilter = document.querySelector('#filter-random');
-// const commentFilter = document.querySelector('#filter-discussed');
-
 
 const response = await fetch('https://28.javascript.pages.academy/kekstagram/data');
 const results = await response.json();
@@ -66,63 +58,7 @@ filterList.addEventListener('click', (evt) => {
 //     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
 //   };
 // };
-// // //results.sort(() => Math.random() - 0.5).slice(0, RANDOM_PICSTURE_COAT);
-// //results.sort((pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length);
-// const renderDefaultPhoto = (photo) => {
-// defaultFilter.addEventListener('click', () => {
-//     defaultFilter.classList.add('img-filters__button--active');
-//     randomFilter.classList.remove('img-filters__button--active');
-//     commentFilter.classList.remove('img-filters__button--active');
-//     pictureRemove();
-//     createPicture(pictureAllPhotoInfo);
-//     //photo();
-//   });
-
-
-//renderDefaultPhoto(debounce(()=>createPicture(pictureAllPhotoInfo), TIME_DELAY));
-
-//const renderRandomPhoto = (photo) => {
-// randomFilter.addEventListener('click', () => {
-//   randomFilter.classList.add('img-filters__button--active');
-//   defaultFilter.classList.remove('img-filters__button--active');
-//   commentFilter.classList.remove('img-filters__button--active');
-//   pictureRemove();
-//   const sortedPictures = pictureAllPhotoInfo
-//     .slice()
-//     .sort(() => Math.random() - 0.5)
-//     .slice(0, RANDOM_PICTURE_COAT);
-//   createPicture(sortedPictures);
-//   //photo();
-
-// });
-//};
-
-//renderRandomPhoto(debounce(()=>createPicture(pictureAllPhotoInfo), TIME_DELAY));
-
-// commentFilter.addEventListener('click', () => {
-//   commentFilter.classList.add('img-filters__button--active');
-//   defaultFilter.classList.remove('img-filters__button--active');
-//   randomFilter.classList.remove('img-filters__button--active');
-//   pictureRemove();
-//   const sortedPictures = pictureAllPhotoInfo.slice().sort((pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length);
-//   createPicture(sortedPictures);
-// });
 
 createPicture(pictureAllPhotoInfo);
-
-// const sortPhotos = (photos) => {
-//   const onFilterClick = (evt) => {
-//     if (evt.target.id === 'filter-default') {
-//     createPicture(photos);
-//     } else if (evt.target.id === 'filter-random') {
-//       photos = results.sort(() => Math.random() - 0.5).slice(0, RANDOM_PICTURE_COAT);
-//       createPicture(photos);
-//     } else if (evt.target.id === 'filter-discussed') {
-//       const sortedPhotosU = photos.slice().sort(sortByComments);
-//       createPicture(sortedPhotosU);
-//     }
-//   };
-//   imgFiltersContainer.addEventListener('click', debounce(onFilterClick, RERENDER_DELAY));
-// };
 
 export {pictures, picture, pictureAllPhotoInfo, results};
