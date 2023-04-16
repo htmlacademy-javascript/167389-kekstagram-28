ffe6a8a91c5b6ecc7958ad19c55c835a845415cf
Eleriniara <eleriniara@gmail.com>
2023-04-16 12:20:11 +0300

    Исправление

--- js/miniphoto.js
+++ js/miniphoto.js
@@ -1,5 +1,5 @@
 const RANDOM_PICTURE_COAT = 10;
-//const TIME_DELAY = 0.5;
+const TIME_DELAY = 0.5;
 
 const pictures = document.querySelector('.pictures');
 const picture = document.querySelector('#picture').content.querySelector('.picture');
@@ -31,33 +31,40 @@ const pictureRemove = () => {
 };
 
 const filterList = document.querySelector('.img-filters');
-filterList.addEventListener('click', (evt) => {
-  document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
-  if(evt.target.classList.contains('img-filters__button')) {
-    evt.target.classList.add('img-filters__button--active');
-    pictureRemove();
-  }
-  if (evt.target.id === 'filter-default') {
-    createPicture(pictureAllPhotoInfo);
-  } else if (evt.target.id === 'filter-random') {
-    const sortedPictures = pictureAllPhotoInfo
-      .slice()
-      .sort(() => Math.random() - 0.5)
-      .slice(0, RANDOM_PICTURE_COAT);
-    createPicture(sortedPictures);
-  } else if (evt.target.id === 'filter-discussed') {
-    const sortedPictures = pictureAllPhotoInfo.slice().sort((pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length);
-    createPicture(sortedPictures);
-  }
-});
+const createFilter = () => {
+  filterList.addEventListener('click', (evt) => {
+    document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+    if(evt.target.classList.contains('img-filters__button')) {
+      evt.target.classList.add('img-filters__button--active');
+      pictureRemove();
+    }
+    if (evt.target.id === 'filter-default') {
+      createPicture(pictureAllPhotoInfo);
+    } else if (evt.target.id === 'filter-random') {
+      const sortedPictures = pictureAllPhotoInfo
+        .slice()
+        .sort(() => Math.random() - 0.5)
+        .slice(0, RANDOM_PICTURE_COAT);
+      createPicture(sortedPictures);
+    } else if (evt.target.id === 'filter-discussed') {
+      const sortedPictures = pictureAllPhotoInfo.slice().sort((pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length);
+      createPicture(sortedPictures);
+    }
+  });
+};
 
-// const debounce = (callback, timeoutDelay) => {
-//   let timeoutId;
-//   return (...rest) => {
-//     clearTimeout(timeoutId);
-//     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
-//   };
-// };
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+createFilter(debounce(
+  () => createPicture(pictureAllPhotoInfo),
+  TIME_DELAY,
+));
 
 createPicture(pictureAllPhotoInfo);
 
